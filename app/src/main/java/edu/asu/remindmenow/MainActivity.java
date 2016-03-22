caedd983ea79f75eb7e25165545c4f23bfca6ddd
jithin <jroy6@asu.edu>
2016-03-21 17:15:34 -0700

    Initial commit

---
+++ app/src/main/java/edu/asu/remindmenow/MainActivity.java
@@ -0,0 +1,85 @@
+package edu.asu.remindmenow;
+
+import android.app.DatePickerDialog;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.DatePicker;
+import android.widget.EditText;
+
+import java.util.Calendar;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    EditText textView;
+    EditText endTextView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_user_reminder);
+
+        textView = (EditText)findViewById(R.id.dateTextView);
+        final Calendar myCalendar = Calendar.getInstance();
+
+        final DatePickerDialog.OnDateSetListener date = new DatePickerDialog.OnDateSetListener() {
+
+            @Override
+            public void onDateSet(DatePicker view, int year, int monthOfYear,
+                                  int dayOfMonth) {
+                // TODO Auto-generated method stub
+                myCalendar.set(Calendar.YEAR, year);
+                myCalendar.set(Calendar.MONTH, monthOfYear);
+                myCalendar.set(Calendar.DAY_OF_MONTH, dayOfMonth);
+                textView.setText(monthOfYear+"/"+dayOfMonth+"/"+year);
+            }
+
+        };
+
+        textView.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                // TODO Auto-generated method stub
+                System.out.println("Inside onclick");
+                new DatePickerDialog(MainActivity.this, date, myCalendar
+                        .get(Calendar.YEAR), myCalendar.get(Calendar.MONTH),
+                        myCalendar.get(Calendar.DAY_OF_MONTH)).show();
+            }
+        });
+
+
+        endTextView = (EditText)findViewById(R.id.endDateTextView);
+        final Calendar myEndCalendar = Calendar.getInstance();
+
+        final DatePickerDialog.OnDateSetListener endDate = new DatePickerDialog.OnDateSetListener() {
+
+            @Override
+            public void onDateSet(DatePicker view, int year, int monthOfYear,
+                                  int dayOfMonth) {
+                // TODO Auto-generated method stub
+                myCalendar.set(Calendar.YEAR, year);
+                myCalendar.set(Calendar.MONTH, monthOfYear);
+                myCalendar.set(Calendar.DAY_OF_MONTH, dayOfMonth);
+                endTextView.setText(monthOfYear+"/"+dayOfMonth+"/"+year);
+            }
+
+        };
+
+        endTextView.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                // TODO Auto-generated method stub
+                System.out.println("Inside onclick");
+                new DatePickerDialog(MainActivity.this, endDate, myEndCalendar
+                        .get(Calendar.YEAR), myCalendar.get(Calendar.MONTH),
+                        myEndCalendar.get(Calendar.DAY_OF_MONTH)).show();
+            }
+        });
+
+
+    }
+
+}
